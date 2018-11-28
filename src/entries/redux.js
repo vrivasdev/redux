9021b3ecce9d8a10f646a763cb38d452a47d9f23
Victor Rivas <vrivas@teravisiontech.com>
2018-11-28 15:16:45 -0400

    Updating app each time the subscribe changes

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -3,11 +3,12 @@ import { createStore } from 'redux';
 const $form = document.getElementById('form');
 $form.addEventListener('submit', handleSubmit);
 
+// ********** 1. UI **********
 function handleSubmit(event) {
   event.preventDefault();
   const data = new FormData($form);
   const title = data.get('title');
-  store.dispatch({ // actions
+  store.dispatch({ // ********* 2. ACTIONS ********
     type: 'ADD_SONG', // mandatory
     payload: {
       title // ES6: title is the same key. Ex: 'title': title
@@ -27,7 +28,7 @@ const initialState = [
   },
 ]
 
-const reducer = function(state, action) {
+const reducer = function(state, action) { // ********** 3. REDUCER *********
   switch (action.type) {
     case 'ADD_SONG':
       return [...state, action.payload]
@@ -36,19 +37,35 @@ const reducer = function(state, action) {
   }
 }
 
-const store = createStore(
+const store = createStore( // ****** 4. STORE **********
   reducer,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
-const $container = document.getElementById('playlist');
-const playlist = store.getState();
+function render() {
+  const $container = document.getElementById('playlist');
+  const playlist = store.getState();
 
-playlist.forEach((item) => {
-  const template = document.createElement('p');
-  template.textContent = item.title;
-  $container.appendChild(template);
-});
+  $container.innerHTML = '';
+
+  playlist.forEach((item) => {
+    const template = document.createElement('p');
+    template.textContent = item.title;
+    $container.appendChild(template);
+  });
+}
+
+render(); // first render
+
+function handleChange() { // render per each change
+  render();
+}
+
+/* NOTE: Updating app with subscribe:
+ * Every time the state gets updated the UI is going to be rendered again.
+ * This is the last step of the redux cicle
+ */
+store.subscribe(handleChange);
 
 console.log(store.getState());
