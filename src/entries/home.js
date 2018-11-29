b75b8d6c920d53333eddf2ce3c1bbf3428547aec
Victor Rivas <vrivas@teravisiontech.com>
2018-11-29 15:30:03 -0400

    Store integration with Provider

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { hydrate } from 'react-dom';
+import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-// import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
 const initialState = {
@@ -21,6 +21,8 @@ console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
-// ReactDOM.render(que voy a renderizar, donde lo haré);
-// const holaMundo = <h1>hola Estudiante!</h1>;
-hydrate( <Home data={data} />, homeContainer);
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>
+, homeContainer);
