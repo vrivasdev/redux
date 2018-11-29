77fe7d07f164269c47098a7598e1c69ff8a244e7
Victor Rivas <vrivas@teravisiontech.com>
2018-11-29 11:17:24 -0400

    Adding store

--- src/entries/home.js
+++ src/entries/home.js
@@ -3,11 +3,24 @@ import { hydrate } from 'react-dom';
 import Home from '../pages/containers/home';
 // import Playlist from './src/playlist/components/playlist';
 import data from '../api.json';
-// console.log('Hola mundo!' )
+import { createStore } from 'redux';
+
+const initialState = {
+  data: {
+    ...data
+  }
+}
+
+const store = createStore(
+  (state) => state, // reducer
+  initialState , // initial state
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // enhancer
+);
+
+console.log(store.getState());
 
 const homeContainer = document.getElementById('home-container')
 
 // ReactDOM.render(que voy a renderizar, donde lo haré);
 // const holaMundo = <h1>hola Estudiante!</h1>;
 hydrate( <Home data={data} />, homeContainer);
-
